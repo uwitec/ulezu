054190302902d1b5bd19ad48376b357e9ae27cc8
shuonar <shuonar@fb9af177-783c-1143-65f8-9ec1721024d6>
2014-03-10 14:21:05 +0000

    房屋信息操作类

---
+++ Ulezu/src/ulezu/com/business/BHouseInfo.java
@@ -0,0 +1,46 @@
+package ulezu.com.business;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import ulezu.com.connection.ConnectionFactory;
+import ulezu.com.factory.DaoFactory;
+import ulezu.com.idao.IHouseInfo;
+import ulezu.com.model.MHoiuseInfo;
+
+/**
+ * 房屋信息业务类
+ * @author Administrator
+ *
+ */
+public class BHouseInfo {
+	/**
+	 * 数据库连接
+	 */
+	private Connection con;
+	
+	/**
+	 * 房屋信息处理类
+	 */
+	private IHouseInfo houseInfoHandler;
+	
+	public BHouseInfo(){
+		this.houseInfoHandler = DaoFactory.getDHouseInfo();
+	} 
+	/**
+	 * 根据id获取房屋信息
+	 * @param id ID
+	 * @return 返回房屋信息
+	 * @throws SQLException 抛出异常
+	 */
+	public MHoiuseInfo getHouseInfoById(String id) throws SQLException {
+		try{
+			this.con = ConnectionFactory.getUlezuReadConnection();
+			return this.houseInfoHandler.getHouseInfoById(id, this.con);
+		}finally{
+			if(!this.con.isClosed()){
+				this.con.close();
+			}
+		}
+	}
+}
