efb34fd7515d1c0db1dd0e9cbdf5a686fbe4fb03
shuonar <shuonar@fb9af177-783c-1143-65f8-9ec1721024d6>
2014-03-15 02:17:10 +0000

    查询

--- trunk/Ulezu/src/ulezu/com/business/BHouseInfo.java
+++ trunk/Ulezu/src/ulezu/com/business/BHouseInfo.java
@@ -2,6 +2,7 @@ package ulezu.com.business;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 import ulezu.com.connection.ConnectionFactory;
 import ulezu.com.factory.DaoFactory;
@@ -60,6 +61,24 @@ public class BHouseInfo {
 			String countStr = this.houseInfoHandler.getAccessCountById(id, this.con);
 			Integer count = Integer.parseInt("".equals(countStr) == true ? "0" : countStr) + 1;
 			return this.houseInfoHandler.updateAccessCountById(id, count.toString(), this.con);
+		}finally{
+			if(!this.con.isClosed()){
+				this.con.close();
+			}
+		}
+	}
+	
+	/**
+	 * 首页搜索
+	 * @param condition 搜索条件 
+	 * @param type 搜索条件（0-商圈，1-学校，2-医院，3-小区，4-景点，5位置）
+	 * @return 返回搜索信息列表
+	 * @throws Exception 抛出异常
+	 */
+	public List<MHouseInfo> homeQuery(String condition, int type) throws Exception{
+		try{
+			this.con = ConnectionFactory.getUlezuReadConnection();
+			return this.houseInfoHandler.homeQuery(condition, type, this.con);
 		}finally{
 			if(!this.con.isClosed()){
 				this.con.close();
